33e211e4321f6c7c3e929b57b74906e8f0f9707d
irishmaiden18 <laurapohl18@gmail.com>
2026-04-16 11:17:39 -0400

    adds counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react"
 
 const Counter = () => {
+
+    const [count, setCount] = useState(0)
+    const [double, setDouble] = useState(0)
+
+    useEffect(() => {
+        setDouble(2 * count)
+    }, [count])
+
   return (
     <>
     <h2>Counter</h2>
+    <p>Count: {count}</p>
+    <p>Double: {double}</p>
+    <button onClick={() => setCount(count+1)}>Add One</button>
     </>
   )
 }
